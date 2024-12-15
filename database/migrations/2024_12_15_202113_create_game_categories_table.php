9656eff964b202eed88827d66ddf37b4d89e424f
MaryamTarek03 <mariamtarek2233@gmail.com>
2024-12-15 23:58:05 +0200

    DatabaseCreated

---
+++ database/migrations/2024_12_15_202113_create_game_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_categories');
+    }
+};
---
+++ database/migrations/2024_12_15_202304_create_list_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('list_games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->unsignedBigInteger('list_id');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->foreign('list_id')->references('id')->on('lists');
+
+            $table->date('add_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('list_games');
+    }
+};
